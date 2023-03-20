6129f4fb0ebd59c5b68a416857939fb9ca898f49
”YevgenyDyachenko” <yevgeny.dyachenko@gmail.com>
2023-03-20 23:08:24 +0200

    first

---
+++ APro/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //System.out.println(2);
+        int a = 1;
+        int b = 2;
+        //add two numbers
+        int c = a + b;
+        //print number c
+        System.out.println(c);
+    }
+}
